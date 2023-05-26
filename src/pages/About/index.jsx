55bf77a3528f1d4e668f6d7d4513ae852907c03e
MatheusGesser <matheusmvg@hotmail.com>
2023-05-26 12:30:43 -0300

    style: skills section

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,16 +2,6 @@ import './styles.scss'
 // import { useAnimations } from './useAnimations'
 import ProfilePic from '../../assets/profile.jpg'
 
-import ReactLogo from '../../assets/skills/react.jpg'
-import JavaScriptLogo from '../../assets/skills/javascript.jpg'
-import TypeScriptLogo from '../../assets/skills/typescript.jpg'
-import GitLogo from '../../assets/skills/git.jpg'
-import HTML5Logo from '../../assets/skills/html5.jpg'
-import CSS3Logo from '../../assets/skills/css3.jpg'
-import SassLogo from '../../assets/skills/sass.jpg'
-import StyledComponentsLogo from '../../assets/skills/styled-components.jpg'
-
-
 export function About({ theme, t }) {
   // useAnimations()
 
@@ -22,16 +12,15 @@ export function About({ theme, t }) {
         <p><span>❝</span>{t('about')}<span>❞</span></p>
       </section>
       <section className="content">
-        <h3>{t('skills')}</h3>
         <section className="skills">
-          <img src={ReactLogo} alt="React logo" />
-          <img src={JavaScriptLogo} alt="React logo" />
-          <img src={TypeScriptLogo} alt="React logo" />
-          <img src={GitLogo} alt="React logo" />
-          <img src={HTML5Logo} alt="React logo" />
-          <img src={CSS3Logo} alt="React logo" />
-          <img src={SassLogo} alt="React logo" />
-          <img src={StyledComponentsLogo} alt="React logo" />
+          <h2 className='major'>React</h2>
+          <h2 className='major'>JavaScript</h2>
+          <h2>TypeScript</h2>
+          <h2>Git</h2>
+          <h2>HTML</h2>
+          <h2 className='major'>CSS</h2>
+          <h2>Sass</h2>
+          <h2>StyledComponents</h2>
         </section>
       </section>
     </main>
